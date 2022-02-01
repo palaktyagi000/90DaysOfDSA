fd1ceaad51dddf9f3300e6ab1af0fa3d715081ab
Palak <palaktyagi000@gmail.com>
2022-02-01 13:31:08 +0530

    DAY 3

---
+++ Stack/balancedPara.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValidExp(char * s){
+
+	stack<char> stk;
+
+	for(int i=0;s[i]!='\0';i++){
+		char ch = s[i];
+
+		if(ch=='('){
+			stk.push(ch);
+		}
+		else if(ch==')'){
+			if(stk.empty() or stk.top()!='('){
+				return false;
+			}
+			stk.pop();
+		}
+	}
+
+
+	return stk.empty();
+
+}
+
+
+
+
+int main(){
+
+	char s[1000] = '((a+b)+(c-d+f))';
+	if(isValidExp(s)){
+		cout<<"Yes";
+	}
+	else{
+		cout<<"No";
+	}
+
+	return 0;
+} 
